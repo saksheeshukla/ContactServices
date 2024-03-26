118eeb4c6b5b5e18c72a2fa85355d2d99af1fa18
Gaurav Yadav <gauravyatwork@gmail.com>
2024-03-26 23:01:59 +0530

    added timestamp to the contacts db

--- models/contacts.model.js
+++ models/contacts.model.js
@@ -1,14 +1,15 @@
 const mongoose = require('./database.js');
 
-//ContactSchema
+// ContactSchema
 const contactSchema = new mongoose.Schema({
     formData: Object,
     user: { 
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: 'User' 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User' 
     },
-  });
-
+}, {
+    timestamps: true // Add timestamps for createdAt and updatedAt
+});
 
 const Contact = mongoose.model('Contact', contactSchema);
 
